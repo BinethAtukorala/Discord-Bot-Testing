6c57fa6524a3e236ddf5f0882ac2f20205e6c6f3
Bineth <bineth.mandiv@gmail.com>
2020-07-26 21:07:56 +0530

    Example use of discord API's utility functions

---
+++ Concepts/utility_functions.py
@@ -0,0 +1,24 @@
+import os
+import discord
+
+TOKEN = "NzM2NjU1MDc3NTE0OTM2MzMw.Xxx9gA.DdpWNklnIDLrW8ThrFhkxML6_20"
+GUILD = "Bineth's Test Bot"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+
+#    for guild in client.guilds:
+#        if guild.name == GUILD:
+#            break
+
+#   The above loop can be replaced for a more intuitive, abstracted utility :- discord.utils.find()
+
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+
+    print(
+        f"{client.user} is connected to the guild {guild.name}(id: {guild.id})"
+    )
+
+client.run(TOKEN)
