43cc0a010bda8f6b7f278e85bd79afbaa586c6f5
Bineth <bineth.mandiv@gmail.com>
2020-07-26 19:37:37 +0530

    Resources and Events
    Added resources file.
    Learned what events are.

---
+++ Concepts/whatareevents.py
@@ -0,0 +1,13 @@
+# DiscordPy revolves around the concept of events. An event is something you listen and respond to.
+
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run('my token goes here')
