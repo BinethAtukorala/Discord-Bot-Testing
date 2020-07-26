56407f83a948f564423b400dd0de937f0dc7fe4a
Bineth <bineth.mandiv@gmail.com>
2020-07-26 20:59:31 +0530

    Made first_bot.py to connect to a guild (server), get it's name, id and users joined to the guild

---
+++ first_bot.py
@@ -0,0 +1,23 @@
+import os
+
+import discord
+
+TOKEN = "NzM2NjU1MDc3NTE0OTM2MzMw.Xxx9gA.DdpWNklnIDLrW8ThrFhkxML6_20"
+GUILD = "Bineth's Test Bot"
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+client.run(TOKEN)
