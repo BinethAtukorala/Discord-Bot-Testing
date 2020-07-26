48a8e4b8bd63afca88db751fdd77cbe3676b881d
Bineth <bineth.mandiv@gmail.com>
2020-07-26 22:09:00 +0530

    Added responding to messages

--- greeter.py
+++ greeter.py
@@ -1,3 +1,4 @@
+import random
 import discord
 
 TOKEN = "NzM2NjU1MDc3NTE0OTM2MzMw.Xxx9gA.DdpWNklnIDLrW8ThrFhkxML6_20"
@@ -14,5 +15,23 @@ async def on_member_join(member):
     await member.dm_channel.send(
         f"Hi {member.name}, welcome to my Discord server! Where we test my Bineth's test bot to it's limits"
     )
+
+@client.event
+async def on_message(message):
+    if(message.author == client.user):
+        return
+
+    brooklyn_99_quotes = [
+        "I'm the human form of the 💯 emoji.",
+        "Bingpot!",
+        (
+            "Cool. Cool cool cool cool cool cool cool, "
+            "no doubt no doubt no doubt no doubt"
+        )
+    ]
+
+    if message.content == "99!":
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
 
 client.run(TOKEN)
